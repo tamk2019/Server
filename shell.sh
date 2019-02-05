1bd9bfad2a196d0a54c601a5ac70db8a09d2f8c8
UrbanTurban <jens.vonweissenberg@tuni.fi>
2019-02-05 18:03:38 +0200

    hopla

--- shell.sh
+++ shell.sh
@@ -2,3 +2,11 @@
 # This is a comment!
 # Tällä shell skriptillä luodaan etälaitteelle palvelinympäristö
 echo Hello World
+echo "What is your name?"
+read USER_NAME
+echo "Hello $USER_NAME"
+echo "What is your quest?"
+read QUEST
+echo "What is the average flight velocity of an unlaiden swallow?"
+echo "I will create you a file called ${USER_NAME}_file, oh noble knight of ${QUEST}"
+touch "${USER_NAME}_file"

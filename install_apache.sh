76768b02ec3f10cba44826a48a4ed66d9563dbb9
Soappi <miiro.opas@gmail.com>
2019-02-12 13:02:22 +0200

    Committing shell scripts

---
+++ install_apache.sh
@@ -0,0 +1,6 @@
+﻿#!/bin/sh
+sudo apt-get install apache2 -y
+# Avaa alla oleva kommentti auki, jos haluat asennuksen jalkeen poistaa apachen
+service apache2 start
+service apache2 stop
+sudo apt-get --purge remove apache2 -y
---
+++ raspi_ssh.sh
@@ -0,0 +1,5 @@
+﻿#!/bin/sh
+echo 'Ladataan ja asennetaan sshpass helppoa yhteyden luontia varten..'
+sudo apt-get install sshpass -y
+echo 'Asennus valmis!'
+sshpass -p 'Tamk1234' ssh pi@89.106.37.88 < install_apache.sh

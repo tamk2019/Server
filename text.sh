eef170a329aa2ddab8d87b50fff3228a79b48d30
tamk2019 <antti.jaakola@tuni.fi>
2019-02-12 18:20:39 +0200

    Updated shitty text editor with more streamline remove

--- text.sh
+++ text.sh
@@ -4,7 +4,7 @@ then
         echo File not found: creating
         touch text.txt
 fi
-echo Input text to append type /q to quit, /r to read, /rs to remove line or /e to erase file content:
+echo Input text to append type /q to quit, /r to read, /rs *string to remove lines with *string or /e to erase file content:
 read TEXT
 while [[ $TEXT != "/q" ]]; do
   if [[ $TEXT == "/e" ]]; then
@@ -16,6 +16,10 @@ while [[ $TEXT != "/q" ]]; do
   elif [[ $TEXT == "/rs" ]]; then
     echo Input part of the line to remove
     read TEXT
+    echo "$(grep -v "$TEXT" text.txt)" >text.txt
+    echo Erased lines containing: $TEXT
+  elif [[ $TEXT == /rs* ]]; then
+    TEXT=${TEXT#"/rs "}
     echo "$(grep -v "$TEXT" text.txt)" >text.txt
     echo Erased lines containing: $TEXT
   else
